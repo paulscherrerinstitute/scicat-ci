096f1d1fc40097fe18693ac89a2ef47430fd0ea1
minotti_c <carlo.minotti@psi.ch>
2022-04-01 11:11:53 +0200

    Fix base url in fe conf files

--- helm_configs/frontend/qa/environment.ts
+++ helm_configs/frontend/qa/environment.ts
@@ -7,7 +7,7 @@ import { TableColumn } from "state-management/models";
 
 export const environment = {
   production: true,
-  lbBaseURL: "https://scicat-prod.qa.psi.ch",
+  lbBaseURL: "https://scicat.qa.psi.ch",
   archiveWorkflowEnabled: true,
   retrieveDestinations: [],
   externalAuthEndpoint: "/auth/msad",
