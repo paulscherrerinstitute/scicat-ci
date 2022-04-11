6dfaf352b558d330144ee435023fb05b781ab9f6
minotti_c <carlo.minotti@psi.ch>
2022-04-11 16:30:36 +0200

    Do not store credentials in userInfo

--- helm_configs/backend/login-callbacks.js
+++ helm_configs/backend/login-callbacks.js
@@ -1,17 +1,21 @@
+"use strict";
+
 exports.accessGroupsToProfile =
-function (req, done) {
-  return function (err, user, identity, token) {
-      identity.updateAttribute('profile', {
-        accessGroups: identity.profile._json.pgroups,
-        email: identity.profile._json.email, 
-        ...identity.profile,
-      })
+  function (req, done) {
+    return function (err, user, identity, token) {
+      identity.updateAttributes({ 
+        "profile": {
+          accessGroups: identity.profile._json.pgroups,
+          email: identity.profile._json.email,
+          ...identity.profile
+        }, 
+        "credentials": null });
       var authInfo = {
         identity: identity,
       };
       if (token) {
         authInfo.accessToken = token;
-      }  
-      done(err, user, authInfo)  
-    }
-};
+      }
+      done(err, user, authInfo);
+    };
+  };
