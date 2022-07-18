9f3330dbd5d9f0bc623cc51f1661b1316ce13ac3
minotti_c <carlo.minotti@psi.ch>
2022-07-18 14:00:44 +0200

    Add deployment of landing page on dev with config

---
+++ helm_configs/landing-page-server/development/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    accessDataHref:
+      "https://www.psi.ch/en/photon-science-data-services/slsswissfel-data-transfer",
+    accessInstructions:
+      "To access the data associated with this DOI click below and follow the instructions",
+    directMongoAccess: true,
+    doiBaseUrl: "https://doi.org/",
+    facility: "psi",
+    lbBaseURL: "https://dacat-development.psi.ch",
+    oaiProviderRoute: null,
+    production: true,
+    scicatBaseUrl: "https://discovery-development.psi.ch",
+    showLogoBanner: false
+  };
