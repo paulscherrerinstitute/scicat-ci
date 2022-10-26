5c5a99a7d30a4043538bb9c0b5512b8f2324e13f
Carlo Minotti <minotti_c@Carlos-MBP.psi.ch>
2022-10-26 12:39:17 +0200

    Add description field

--- scicat-to-pss/copy_public_ds.py
+++ scicat-to-pss/copy_public_ds.py
@@ -1,47 +1,39 @@
-from asyncio import constants
 import logging
 from os import environ
 
 from requests import get, post
 
 meaningful_fields = {
-    'datasets': {
-        'title': 'datasetName',
-        'keywords': 'keywords',
-        'metadata': 'scientificMetadata'
+    "datasets": {
+        "title": "datasetName",
+        "keywords": "keywords",
+        "metadata": "scientificMetadata",
     },
 }
 
 
 def prepFields(item, group):
-    return {
-        k: item.get(v, '')
-        for k, v
-        in meaningful_fields[group].items()
-    }
+    return {k: item.get(v, "") for k, v in meaningful_fields[group].items()}
 
 
 def format_dataset_for_scoring(raw_datasets):
     return [
         {
-            'id': item['pid'],
-            'group': 'datasets',
-            'fields': prepFields(item, 'datasets')
+            "id": item["pid"],
+            "group": "datasets",
+            "description": "description",
+            "fields": prepFields(item, "datasets"),
         }
-        for item
-        in raw_datasets
+        for item in raw_datasets
     ]
 
 
 def post_datasets_to_scoring(scoring_datasets, pss_items_url):
-    return post(
-        pss_items_url,
-        json=scoring_datasets
-    )
+    return post(pss_items_url, json=scoring_datasets)
 
 
 def compute_weights(pss_compute_url):
-    return get(pss_compute_url)
+    return post(pss_compute_url)
 
 
 def get_public_datasets(sc_datasets_url):
@@ -66,4 +58,4 @@ def main(scicat_base_url, pss_base_url):
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    main(environ['SCICAT_BASE_URL'], environ['PSS_BASE_URL'])
+    main(environ["SCICAT_BASE_URL"], environ["PSS_BASE_URL"])
