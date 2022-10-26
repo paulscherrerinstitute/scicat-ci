a754354bba2077845a4320f590c4fe92189d854c
Carlo Minotti <minotti_c@Carlos-MBP.psi.ch>
2022-10-26 12:58:36 +0200

    Delete scored items

--- scicat-to-pss/copy_public_ds.py
+++ scicat-to-pss/copy_public_ds.py
@@ -1,7 +1,7 @@
 import logging
 from os import environ
 
-from requests import get, post
+from requests import get, post, delete
 
 meaningful_fields = {
     "datasets": {
@@ -28,6 +28,16 @@ def format_dataset_for_scoring(raw_datasets):
     ]
 
 
+def delete_all_scored(pss_items_url):
+    res = get(
+        pss_items_url,
+    )
+    delete_codes = map(
+        lambda x: delete(f"{pss_items_url}/{x[id]}").status_code, res.json() or []
+    )
+    return list(delete_codes)
+
+
 def post_datasets_to_scoring(scoring_datasets, pss_items_url):
     return post(pss_items_url, json=scoring_datasets)
 
@@ -46,11 +56,14 @@ def get_public_datasets(sc_datasets_url):
 def main(scicat_base_url, pss_base_url):
     logging.info(scicat_base_url)
     logging.info(pss_base_url)
+    pss_items_url = f"{pss_base_url}/items"
+    delete_status_codes = delete_all_scored(pss_items_url)
+    logging.info(delete_status_codes)
     public_datasets = get_public_datasets(f"{scicat_base_url}/datasets")
     logging.info(len(public_datasets))
     scoring_datasets = format_dataset_for_scoring(public_datasets)
     logging.info(len(scoring_datasets))
-    to_scoring = post_datasets_to_scoring(scoring_datasets, f"{pss_base_url}/items")
+    to_scoring = post_datasets_to_scoring(scoring_datasets, pss_items_url)
     logging.info(to_scoring.json())
     scores = compute_weights(f"{pss_base_url}/compute")
     logging.info(scores.json())
