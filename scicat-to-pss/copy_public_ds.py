478d1bb4694d3eff2a5e77c050dca15f6287c377
Carlo Minotti <minotti_c@Carlos-MBP.yallo.box>
2022-10-26 23:46:57 +0200

    Score documents for real

--- scicat-to-pss/copy_public_ds.py
+++ scicat-to-pss/copy_public_ds.py
@@ -10,6 +10,13 @@ meaningful_fields = {
         "metadata": "scientificMetadata",
         "description": "description",
     },
+    "documents": {
+        "doi": "doi",
+        "creator": "creator",
+        "title": "title",
+        "abstract": "abstract",
+        "description": "datasetDescription"
+    },
 }
 
 
@@ -22,7 +29,7 @@ def format_dataset_for_scoring(raw_datasets, group="datasets"):
         {
             "id": item["pid"],
             "group": group,
-            "fields": prepFields(item, "datasets"),
+            "fields": prepFields(item, group),
         }
         for item in raw_datasets
     ]
@@ -59,16 +66,24 @@ def main(scicat_base_url, pss_base_url):
     pss_items_url = f"{pss_base_url}/items"
     delete_status_codes = delete_all_scored(pss_items_url)
     logging.info(delete_status_codes)
+    
+    # datasets
     public_datasets = get_public_datasets(f"{scicat_base_url}/datasets")
     logging.info(len(public_datasets))
     scoring_datasets = format_dataset_for_scoring(public_datasets)
     logging.info(len(scoring_datasets))
-    scoring_documents = format_dataset_for_scoring(public_datasets, "documents")
-    logging.info(len(scoring_documents))
     to_scoring_datasets = post_datasets_to_scoring(scoring_datasets, pss_items_url)
     logging.info(to_scoring_datasets.json())
+
+    # documents
+    public_documents = get_public_datasets(f"{scicat_base_url}/PublishedData")
+    logging.info(len(public_documents))
+    scoring_documents = format_dataset_for_scoring(public_documents, "documents")
+    logging.info(len(scoring_documents))
     to_scoring_documents = post_datasets_to_scoring(scoring_documents, pss_items_url)
     logging.info(to_scoring_documents.json())
+    
+    # scores
     scores = compute_weights(f"{pss_base_url}/compute")
     logging.info(scores.json())
 
