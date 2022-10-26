daa9cc99653a4843d2ca37ffb18f3694c5d1a4cd
Carlo Minotti <minotti_c@Carlos-MBP.yallo.box>
2022-10-26 23:14:21 +0200

    Add score for documents

--- scicat-to-pss/copy_public_ds.py
+++ scicat-to-pss/copy_public_ds.py
@@ -10,7 +10,50 @@ meaningful_fields = {
         "metadata": "scientificMetadata",
         "description": "description",
     },
+    "documents": {
+        "doi": "doi",
+        "creator": "creator",
+        "title": "title",
+        "abstract": "abstract",
+        "authors": "authors",
+        "datasets": {
+            "owner": "owner",
+            "principal investigator": "principalInvestigator",
+            "keywords": "keywords",
+            "description": "description",
+            "title": "datasetName",
+            "metadata": "scientificMetadata",
+        },
+    },
 }
+
+
+def prepNestedFields(item, fields_list):
+    return {dk: extractFieldValue(dk, sk, item) for dk, sk in fields_list.items()}
+
+
+def extractFieldValue(dk, sk, item):
+    output = ""
+    if type(sk) == dict:
+        if type(item[dk]) == list:
+            output = [prepNestedFields(i, sk) for i in item[dk]]
+        else:
+            output = prepNestedFields(item[dk], sk)
+    elif sk in item.keys():
+        output = item[sk]
+
+    return output
+
+
+def format_documents_for_scoring(raw_datasets):
+    return [
+        {
+            "id": item["doi"],
+            "group": "documents",
+            "fields": prepNestedFields(item, meaningful_fields["documents"]),
+        }
+        for item in raw_datasets
+    ]
 
 
 def prepFields(item, group):
@@ -63,8 +106,12 @@ def main(scicat_base_url, pss_base_url):
     logging.info(len(public_datasets))
     scoring_datasets = format_dataset_for_scoring(public_datasets)
     logging.info(len(scoring_datasets))
-    to_scoring = post_datasets_to_scoring(scoring_datasets, pss_items_url)
-    logging.info(to_scoring.json())
+    scoring_documents = format_documents_for_scoring(public_datasets)
+    logging.info(len(scoring_documents))
+    to_scoring_datasets = post_datasets_to_scoring(scoring_datasets, pss_items_url)
+    logging.info(to_scoring_datasets.json())
+    to_scoring_documents = post_datasets_to_scoring(scoring_documents, pss_items_url)
+    logging.info(to_scoring_documents.json())
     scores = compute_weights(f"{pss_base_url}/compute")
     logging.info(scores.json())
 
