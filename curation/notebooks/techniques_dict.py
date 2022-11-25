b44ac069898f1fdf70fb5046f6de852c5eb31a07
Carlo Minotti <carlo.minotti@psi.ch>
2022-11-25 18:37:49 +0100

    Add curation notebooks for techniques upload

---
+++ curation/notebooks/techniques_dict.py
@@ -0,0 +1,53 @@
+location_panet_dict = {
+    "/PSI/SLS/ADRESS-SX-ARPES": [
+        {
+            "pid": "http://purl.org/pan-science/PaNET/PaNET01015",
+            "name": "soft x-ray probe",
+        }
+    ],
+    "/PSI/SLS/CSAXS": [
+        {
+            "pid": "http://purl.org/pan-science/PaNET/PaNET01020",
+            "name": "elastic scattering",
+        }
+    ],
+    "/PSI/SLS/IR/FTIR/IFS125": [
+        {
+            "pid": "http://purl.org/pan-science/PaNET/PaNET01320",
+            "name": "fourier transform infrared spectroscopy",
+        }
+    ],
+    "/PSI/SLS/MX": [
+        {
+            "pid": "http://purl.org/pan-science/PaNET/PaNET01164",
+            "name": "macromolecular crystallography",
+        }
+    ],
+    "/PSI/SLS/SIM": [
+        {
+            "pid": "http://purl.org/pan-science/PaNET/PaNET01146",
+            "name": "photoemission microscopy",
+        }
+    ],
+    "/PSI/SLS/TOMCAT": [
+        {"pid": "http://purl.org/pan-science/PaNET/PaNET01129", "name": "tomography"}
+    ],
+    "/PSI/SLS/VUV": [
+        {"pid": "http://purl.org/pan-science/PaNET/PaNET00100", "name": "photon probe"}
+    ],
+    "/PSI/SLS/cSAXS": [
+        {
+            "pid": "http://purl.org/pan-science/PaNET/PaNET01020",
+            "name": "elastic scattering",
+        }
+    ],
+    "/PSI/SMUS/LEM": [
+        {"pid": "http://purl.org/pan-science/PaNET/PaNET00102", "name": "muon probe"}
+    ],
+    "/PSI/SWISSFEL/ARAMIS-ALVRA": [
+        {"pid": "http://purl.org/pan-science/PaNET/PaNET01012", "name": "x-ray probe"}
+    ],
+    "/PSI/SmuS/piE1/muX": [
+        {"pid": "http://purl.org/pan-science/PaNET/PaNET00102", "name": "muon probe"}
+    ],
+}
