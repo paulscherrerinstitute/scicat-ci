c22953e6a665e7e9701160a66a0b92a6272064e0
minottic <carlo.minotti1@gmail.com>
2023-03-17 18:52:11 +0100

    Fetch user first

--- helm/configs/backend/authorizedJobs.js
+++ helm/configs/backend/authorizedJobs.js
@@ -31,34 +31,45 @@ const removeNonAuthorized = (resultList, currentUser) => (
   resultList.filter(item => item.emailJobInitiator === currentUser)
 )
 
+const getCurrentUserEmail = async (userId) => {
+  const userIdentityModel = app.models.UserIdentity;
+  const userIdentity = await userIdentityModel.find({userId: userId});
+  if (userIdentity.length === 1 && userIdentity.profile && userIdentity.profile.email)
+    return userIdentity.profile.email
+  const userModel = app.models.User;
+  const user = await userModel.findById(userId);
+  return user.email
+}
+
 module.exports = function (app) {
-  app.models.Job.beforeRemote("**",function(ctx, unused, next){
+  app.models.Job.beforeRemote("**", async (ctx, unused, next) => {
     if (isGlobalAccess(ctx.args.options.currentGroups)) {
       next();
       return;
     }
-    if (ctx.args.data && ctx.methodString !== "Job.create")
-      checkEmailJobInitiator(ctx.args.data, ctx.args.options.currentUserEmail);
-    else if ("filter" in ctx.args) {
+    if (ctx.args.data && ctx.methodString !== "Job.create") {
+      const email = await getCurrentUserEmail(ctx.options.accessToken.userId);
+      checkEmailJobInitiator(ctx.args.data, email);
+    } else if ("filter" in ctx.args) {
+      const email = await getCurrentUserEmail(ctx.options.accessToken.userId);
       ctx.args.filter = addEmailJobInitiatorFilter(
         ctx.filter, 
-        ctx.args.options.currentUserEmail
+        email
       );
     }
-    next();
   });
 
-  app.models.Job.afterRemote("**",function(ctx, unused, next){
+  app.models.Job.afterRemote("**", async (ctx, unused, next) => {
     if (isGlobalAccess(ctx.args.options.currentGroups)) {
       next();
       return;
     }
     if(ctx.result && ctx.methodString !== "Job.create") {
+      const email = await getCurrentUserEmail(ctx.args.options.accessToken.userId);
       if (ctx.args.id)
-        checkEmailJobInitiator(ctx.result, ctx.args.options.currentUserEmail);
+        checkEmailJobInitiator(ctx.result, email);
       else if (Array.isArray(ctx.result)) 
-        ctx.result = removeNonAuthorized(ctx.result, ctx.args.options.currentUserEmail)
+        ctx.result = removeNonAuthorized(ctx.result, email)
     }
-    next();
   });
 }
