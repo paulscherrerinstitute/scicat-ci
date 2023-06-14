97df52e7eacf5710db7975001da8a94396078720
minottic <carlo.minotti1@gmail.com>
2023-06-14 10:12:57 +0200

    Move js and sh to helm config

--- matviews/updateMatViews.sh
+++
@@ -1,2 +0,0 @@
-mongosh 'mongodb://$(USER):$(PASSWORD)@my-cluster-name-rs0.mongo.svc.cluster.local/$(DB)?replicaSet=rs0&directConnection=true&readPreference=secondaryPreferred' --file updateMatViews.js  
-
