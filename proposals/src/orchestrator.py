a453124d3da87ef03d7edb0eb9063a6c52399d6d
minottic <carlo.minotti1@gmail.com>
2025-07-18 18:35:13 +0200

    Add orchestrator class

---
+++ proposals/src/orchestrator.py
@@ -0,0 +1,69 @@
+import os
+from abc import ABCMeta, abstractmethod
+from datetime import datetime
+
+from dotenv import load_dotenv
+
+from proposals import ProposalsFromFacility, ProposalsFromPgroups
+from scicat import SciCatAuth, SciCatPolicyFromDuo, SciCatProposalFromDuo
+from utils import log
+
+
+class Orchestrator(metaclass=ABCMeta):
+
+    @abstractmethod
+    def orchestrate(self):
+        raise NotImplementedError
+
+
+class DuoSciCatOrchestrator(Orchestrator):
+
+    def __init__(self):
+        load_dotenv()
+        # scicat config variables
+        SCICAT_ENDPOINT = os.environ["SCICAT_ENDPOINT"]
+        SCICAT_USERNAME = os.environ["SCICAT_USERNAME"]
+        SCICAT_PASSWORD = os.environ["SCICAT_PASSWORD"]
+
+        # duo config variables
+        DUO_ENDPOINT = os.environ["DUO_ENDPOINT"]
+        DUO_SECRET = os.environ["DUO_SECRET"]
+        DUO_YEAR = os.environ["DUO_YEAR"]
+        self.year = DUO_YEAR or datetime.now().year
+        DUO_FACILITY = os.environ["DUO_FACILITY"]
+        self.duo_facility = DUO_FACILITY
+
+        self.scicat_instance = SciCatAuth(
+            SCICAT_USERNAME, SCICAT_PASSWORD, SCICAT_ENDPOINT
+        )
+        self.duo_instance = {"pgroups": ProposalsFromPgroups}.get(
+            DUO_FACILITY, ProposalsFromFacility
+        )(DUO_ENDPOINT, DUO_SECRET)
+
+    @staticmethod
+    def _upsert_policy_and_proposal(policy, proposal):
+        try:
+            # check for existence of Proposal data and merge schedules into it
+            proposal.update()
+        except SciCatProposalFromDuo.ProposalNotFoundException as e:
+            log.error(e)
+            # create new proposal
+            proposal.create()
+            policy.create()
+
+    def _upsert_policy_and_proposal_from_duo(self, duo_proposal, accelerator):
+        log.info(f"============= Input proposal: {duo_proposal['proposal']}")
+        policy = SciCatPolicyFromDuo(duo_proposal, accelerator)
+        proposal = SciCatProposalFromDuo(duo_proposal, accelerator, self.duo_facility)
+        self._upsert_policy_and_proposal(policy, proposal)
+
+    def orchestrate(self):
+        log.info(
+            f"Fetching proposals for accelerator {self.duo_facility} and year {self.year}"
+        )
+        log.info(f"Connecting to scicat on {self.scicat_instance.url}")
+        self.scicat_instance.authenticate()
+        for proposal, facility in self.duo_instance.proposals(
+            self.duo_facility, self.year
+        ):
+            self._upsert_policy_and_proposal_from_duo(proposal, facility)
