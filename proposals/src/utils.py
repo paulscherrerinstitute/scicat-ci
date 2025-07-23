c7fa06b4758040585f0d3f68c0f7c880e71854fa
minottic <carlo.minotti1@gmail.com>
2025-07-23 09:24:15 +0200

    Add utils docstrings

--- proposals/src/utils.py
+++ proposals/src/utils.py
@@ -1,12 +1,27 @@
 from functools import wraps
-from logging import getLogger
+from logging import Logger, getLogger
 from time import sleep
+from typing import Any, Callable, Optional, TypeVar
 from urllib.error import URLError
 
+log: Logger = getLogger("duo_sync")
 
-def retry(func):
+F = TypeVar("F", bound=Callable[..., Any])
+
+
+def retry(func: F) -> F:
+    """
+    Decorator that retries a function up to 5 times if it raises a URLError.
+
+    Args:
+        func (Callable): The function to retry.
+
+    Returns:
+        Callable: A wrapped version of the function that will retry on URLError.
+    """
+
     @wraps(func)
-    def wrap(*args, **kwargs):
+    def wrap(*args: Any, **kwargs: Any) -> Optional[Any]:
         for n in range(5):
             try:
                 log.info(f"HTTP call attempt {n}")
@@ -14,13 +29,11 @@ def retry(func):
             except URLError as e:
                 log.warning(e)
                 if n == 4:
-                    log.error(f"HTTP last call attempt failed")
+                    log.error("HTTP last call attempt failed")
                     return
                 sleep(n * 10 or n)
             else:
                 return res
+        return None
 
-    return wrap
-
-
-log = getLogger("duo_sync")
+    return wrap  # type: ignore
