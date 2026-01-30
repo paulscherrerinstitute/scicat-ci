30bb0a19c5f93e0150071262d17cc4eefc8918f7
Omkar Zade <omkar.zade@psi.ch>
2026-01-30 13:36:42 +0100

    batch replaceOne ops and use bulkWrite and address comments

--- helm/configs/backend-next/migrate-broken-legacy-history.js
+++ helm/configs/backend-next/migrate-broken-legacy-history.js
@@ -2,6 +2,12 @@
 const convertObsoleteHistoryToGenericHistory =
   require("../dist/datasets/utils/history.util").convertObsoleteHistoryToGenericHistory;
 
+/**This script migrates v3 history records that were in invalid format to the generic History collection.
+ * Instead of the expected structure {datasetlifecycle: {previousValue: ..., currentValue: ...}}, some history records
+ * only contained a {datasetlifecycle: updatedValue}. As a result, these were missed by the original migration script
+ * i.e. 20251014092142-legacy-history-to-generic-history.js.
+ * This script reconstructs the broken records in expected format, and migrates them to generic history.
+ */
 module.exports = {
   /**
    * @param db {import('mongodb').Db}
@@ -10,7 +16,7 @@ module.exports = {
    */
   async up(db, client) {
     // The query on line 14 should be run against the old DB snapshot
-    // One approach would be to copy the old dacat.Dataset collection into the current DB say under old.Dataset
+    // One approach would be to copy the old dacat.Dataset collection into the current DB say under oldDataset
     const affectedDatasets = db.collection("oldDataset").find({
       history: {
         $elemMatch: {
@@ -20,6 +26,8 @@ module.exports = {
       },
     });
 
+    let batch = [];
+    const BATCH_SIZE = 1000;
     let successCount = 0;
     let failedCount = 0;
     for await (const ds of affectedDatasets) {
@@ -40,44 +48,46 @@ module.exports = {
             entry,
             pid,
           );
-          // find the generic history record in db with the same timestamp and empty before / after, and replace it with this one
-          const updatedGenericHistory = await db
-            .collection("History")
-            .replaceOne(
-              {
+          batch.push({
+            replaceOne: {
+              filter: {
                 documentId: pid,
                 timestamp: genericHistory.timestamp,
                 before: {},
                 after: {},
               },
-              genericHistory,
+              replacement: genericHistory,
+            },
+          });
+          if (batch.length === BATCH_SIZE) {
+            const bulkWriteResult = await db
+              .collection("History")
+              .bulkWrite(batch, { ordered: false });
+            successCount += bulkWriteResult.modifiedCount;
+            failedCount += BATCH_SIZE - bulkWriteResult.modifiedCount;
+
+            batch = [];
+            console.log(
+              "migrating, count, failedCount: ",
+              successCount,
+              failedCount,
             );
-          if (updatedGenericHistory) {
-            successCount++;
-          } else {
-            failedCount++;
           }
         }
       }
-      if (successCount % 1000 == 0) {
-        console.log(
-          "migrating, count, failedCount: ",
-          successCount,
-          failedCount,
-        );
-      }
     }
-    console.log("count, failedCount: ", successCount, failedCount);
+    // Flush remaining writes, if any
+    if (batch.length > 0) {
+      const bulkWriteResult = await db
+        .collection("History")
+        .bulkWrite(batch, { ordered: false });
+      successCount += bulkWriteResult.modifiedCount;
+      failedCount += batch.length - bulkWriteResult.modifiedCount;
+    }
+    console.log("FINISHED: count, failedCount: ", successCount, failedCount);
   },
 
-  /**
-   * @param db {import('mongodb').Db}
-   * @param client {import('mongodb').MongoClient}
-   * @returns {Promise<void>}
-   */
   async down(db, client) {
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+    /* The down function of the original script 20251014092142-legacy-history-to-generic-history.js is sufficient for downward migration*/
   },
 };
